e916a010d571408238969187c28d69333ebd88e6
Jorge Guzman <joguzmandev@gmail.com>
2021-07-30 02:29:58 +0200

    Create State Controller and View to display all states

---
+++ Controllers/StateController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamPlayCRUD.Data;
+using TeamPlayCRUD.Models;
+
+namespace TeamPlayCRUD.Controllers
+{
+    public class StateController : Controller
+    {
+        public TeamPlayerContext db { get; set; }
+        public StateController(TeamPlayerContext context)
+        {
+            this.db = context;
+        }
+        public IActionResult Index()
+        {
+            List<State> states = db.States.ToList();
+            return View(states);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            this.db = null;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TeamPlayCRUD.Data;
 
 namespace TeamPlayCRUD
 {
@@ -24,6 +25,7 @@ namespace TeamPlayCRUD
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<TeamPlayerContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
